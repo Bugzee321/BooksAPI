e6c31ae9ea6ed79c4af688c81681ee5cc003d692
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-03-17 20:40:19 +0200

    Feat: Remove request validation

--- app/Http/Controllers/API/BooksController.php
+++ app/Http/Controllers/API/BooksController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ShowBookRequest;
 use App\Services\OpenLibraryService;
 use Illuminate\Http\Request;
 
